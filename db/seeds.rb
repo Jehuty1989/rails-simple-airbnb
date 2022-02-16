c0b77c2c9600e43e89f5923d7166da2a1f929904
Alastair Turner <specta1989@gmail.com>
2022-02-16 16:45:26 +0000

    Basic skeleton done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Flat.create!(
+    name: Faker::GreekPhilosophers.name,
+    address: Faker::Address.street_address,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: (50..500).to_a.sample,
+    number_of_guests: (1..6).to_a.sample
+  )
+end
